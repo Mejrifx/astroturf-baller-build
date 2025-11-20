a5f02706a6e430a0cdd2fa8ff9f14cb4110bc6ff
Mejri FX <mejrifx@example.com>
2025-11-20 15:44:31 +0000

    Remove splash page and set Home as root route

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Splash from "./pages/Splash";
 import Home from "./pages/Home";
 import NotFound from "./pages/NotFound";
 
@@ -16,8 +15,7 @@ const App = () => (
       <Sonner />
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Splash />} />
-          <Route path="/home" element={<Home />} />
+          <Route path="/" element={<Home />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
