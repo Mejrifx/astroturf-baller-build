468997a93fe3a4e84c923e447bc14d712d7dc359
Mejri FX <mejrifx@example.com>
2025-11-20 22:22:26 +0000

    Replace Messenger SVG with official Messenger logo PNG

--- src/components/MessengerButton.tsx
+++ src/components/MessengerButton.tsx
@@ -7,15 +7,12 @@ const MessengerButton = () => {
       className="fixed bottom-6 right-6 z-50 w-14 h-14 bg-[#0084FF] hover:bg-[#0066CC] rounded-full shadow-2xl flex items-center justify-center transition-all duration-300 hover:scale-110 group"
       aria-label="Contact us on Messenger"
     >
-      {/* Official Messenger Logo SVG - Lightning Bolt */}
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 24 24"
-        className="w-8 h-8 fill-white"
-      >
-        <path d="M12 0C5.373 0 0 5.373 0 12s5.373 12 12 12 12-5.373 12-12S18.627 0 12 0zm0 22C6.477 22 2 17.523 2 12S6.477 2 12 2s10 4.477 10 10-4.477 10-10 10z"/>
-        <path d="M13.5 6.5L9 13h3.5l-1.5 4.5L16.5 11H13l.5-4.5z" fill="white"/>
-      </svg>
+      {/* Official Messenger Logo PNG */}
+      <img 
+        src="/Messenger-logo.png" 
+        alt="Messenger" 
+        className="w-8 h-8 object-contain"
+      />
       
       {/* Pulse animation ring */}
       <span className="absolute inset-0 rounded-full bg-[#0084FF] animate-ping opacity-20" />
